b2c5c79f3b17df71b4e5e64d64d4c2533cd512cf
dauletakberdiyev <dauletakberdiyev@gmail.com>
2025-05-24 14:57:50 +0500

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    {
+      path: '/',
+      redirect: '/welcome' 
+    },
     { 
       path: '/welcome', 
       name: 'welcome',
-      component: () => import('../pages/WelcomePage.vue') }
+      component: () => import('../pages/WelcomePage.vue') 
+    }
   ]
 })
 
