7c4ba22ecd658fb8e8ccdc592e73c002f17f0c33
dauletakberdiyev <dauletakberdiyev@gmail.com>
2025-05-25 19:01:44 +0500

    save user from tg done

--- src/pages/WelcomePage.vue
+++ src/pages/WelcomePage.vue
@@ -20,19 +20,6 @@
 
       getUser(tgUser);
     }
-    // // @ts-ignore
-    // const tgUser = {
-    //   username: 'test1',
-    //   id: '32',
-    //   first_name: 'tes2',
-    //   photo_url: 'test2'
-    // };
-    // // @ts-ignore
-    // username.value = tgUser?.username;
-    // // @ts-ignore
-    // tgId.value = tgUser.id;
-
-    // getUser(tgUser);
   });
 
   async function getUser(tgUser: any) {
